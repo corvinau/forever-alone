12cae49e1029fb928ff0ad5e095a54bc58ccd15e
Jorge Madson <filholouco@gmail.com>
2018-06-12 18:32:47 -0300

    Envio de email quando cadastra

--- src/java/com/ufpr/tads/facades/UsuarioFacade.java
+++ src/java/com/ufpr/tads/facades/UsuarioFacade.java
@@ -3,6 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+ /*LEIA-ME
+ Para fazer o envio de email funcionar
+ 1- baixar o javax.mail.jar desse link: https://javaee.github.io/javamail/
+ 2- Adicionar o javax.mail como library no projeto
+ 3- Feito!
+*/
 package com.ufpr.tads.facades;
 
 import com.ufpr.tads.beans.Cidade;
@@ -22,7 +29,15 @@ import com.ufpr.tads.dao.PreferenciaDAO;
 import com.ufpr.tads.dao.UFDAO;
 import com.ufpr.tads.dao.UsuarioDAO;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -30,6 +45,44 @@ import java.util.Random;
  */
 public class UsuarioFacade {
     
+    //Esse método envia email
+    //Precisa inserir o javax.mail.jar na biblioteca (libraries)
+    private static void sendFromGMail(String para, String assunto, String corpo) {
+        //O usuário que manda o email
+        String from ="foreveralonebot"; //email sem o @gmail.com
+        String pass = "razer2018"; //senha do email
+        
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            
+            InternetAddress toAddress = new InternetAddress(para);
+            message.addRecipient(Message.RecipientType.TO, toAddress);
+            message.setSubject(assunto);
+            message.setText(corpo);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
     public static Usuario usuarioLogin(String email,String senha){
         UsuarioDAO usuarioDao = new UsuarioDAO();
         Usuario u = usuarioDao.getUsuario(email, senha);
@@ -39,6 +92,11 @@ public class UsuarioFacade {
     public static int createCliente(Cliente c){
         if(c.getSenha() == null || c.getSenha().isEmpty()){
             c.setSenha(getRandomString(5));
+        //Envia senha por email
+        String  assunto = "Teste usuário criado";
+        String corpo = "Sua senha é: " + c.getSenha();
+        String email = c.getEmail();
+        sendFromGMail(email, assunto, corpo);
         }
         UsuarioDAO usuarioDao = new UsuarioDAO();
         return usuarioDao.insertUsuario(c);
